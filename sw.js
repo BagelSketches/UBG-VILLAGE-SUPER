353ea595c1b985c557ed11095cda4b4e2728fe3e
Devilapig247 <aidyn.dmello1@gmail.com>
2026-03-05 21:03:48 +1100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,18 @@
-importScripts("/scram/scramjet.all.js");
+importScripts('./scramjet.all.js');
 
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
 
-async function handleRequest(event) {
-	await scramjet.loadConfig();
-	if (scramjet.route(event)) {
-		return scramjet.fetch(event);
-	}
-	return fetch(event.request);
-}
-
-self.addEventListener("fetch", (event) => {
-	event.respondWith(handleRequest(event));
+self.addEventListener('fetch', (event) => {
+    event.respondWith(async () => {
+        // This initializes the proxy logic
+        await scramjet.loadConfig(); 
+        
+        if (scramjet.route(event)) {
+            return scramjet.fetch(event);
+        }
+        
+        // If it's not a proxy request, let the browser handle it normally
+        return fetch(event.request);
+    });
 });
